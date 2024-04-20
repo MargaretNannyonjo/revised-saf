49f9554e4f450f79ed23f26964ef532b87a086aa
MargaretNannyonjo <nannyonjoleon@gmail.com>
2024-04-20 20:04:10 +0300

    User authentication

--- src/components/Authenticaton.js
+++ src/components/Authenticaton.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Authentication = () => {
+  let [error, setError] = useState(false);
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
   return (
     <>
-      <h1>Authentication post</h1>
+      <div className="user-auth">
+        <form onSubmit={handleLogin}>
+          <input
+            type="email"
+            placeholder="Email address"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="button" className="login">
+            LOGIN
+          </button>
+          {error && <span>Wrong email or password!</span>}
+        </form>
+      </div>
     </>
   );
 };
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLrya9NBy6GoQHD1b7ODBl62BvTpqmlh8",
+  authDomain: "safari-food-and-travel.firebaseapp.com",
+  projectId: "safari-food-and-travel",
+  storageBucket: "safari-food-and-travel.appspot.com",
+  messagingSenderId: "319854272795",
+  appId: "1:319854272795:web:abec3b33e878e2434bb3be",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
